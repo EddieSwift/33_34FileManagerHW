7aaa47482cceca79bd841fe9a70736476628dbcb
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-02-08 17:02:30 +0200

    Initial Commit

---
+++ 33_34FileManagerHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  33_34FileManagerHW
+//
+//  Created by Eduard Galchenko on 2/8/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
